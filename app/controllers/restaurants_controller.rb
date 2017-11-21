da2bc9b7c7b1435168610e129661ebd7660ab5d4
David Rossiter <david_rossiter@hotmail.com>
2017-11-21 17:28:57 +0000

    Progress?

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,11 @@
 class RestaurantsController < ApplicationController
+ before_action :find_restaurant, only: [ :show, :destroy, :update ]
+
   def index
     @restaurants = Restaurant.all
   end
 
   def show
-    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -12,6 +13,7 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.create
   end
 
   def edit
@@ -29,5 +31,9 @@ class RestaurantsController < ApplicationController
 
   def restaurant_params
     params.require(:restaurant).permit()
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
