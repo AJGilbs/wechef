3c6e679650761e03d06a7998fee946f9f46acfca
Nathalia de Luccas Filopoulos <nathiluccas@gmail.com>
2017-11-22 12:54:17 +0000

    teste booking

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,16 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+
+  before_action :authenticate_restaurant!, only: :dashboard_restaurant
+  before_action :authenticate_chef!, only: :dashboard_chef
+
   def home
   end
+
+  def dashboard_restaurant
+
+  end
+
+  def dashboard_chef
+  end
+
 end
