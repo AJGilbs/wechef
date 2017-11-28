12c53d1f163b8704f7e07bab66f3ad6ed4000c28
Cristiane Silva <cristiane.hdo@gmail.com>
2017-11-28 13:26:20 +0000

    position is working, need to implement search by

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -1,22 +1,28 @@
 class PositionsController < ApplicationController
 
+
   def create
+    @chef = current_chef
     @position = Position.new(position_params)
     @position.chef = current_chef
     authorize(@position)
-    if not @position.save
-      render edit_chef(@chef)
+    if @position.save
+      render "/chefs/edit"
+    else
+      render "new"
     end
   end
 
   def destroy
+    @chef = current_chef
     @position = Position.find(params[:id])
     authorize(@position)
     @position.destroy
+    redirect_to edit_chef_path(@chef)
   end
 
   private
   def position_params
-    params.require(:position).permit(:title, :description, :start_date, :end_date)
+    params.require(:position).permit(:title, :restaurant_name, :description, :start_date, :end_date)
   end
 end
